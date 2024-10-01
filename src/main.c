9893f016beb8c03342055358f033204c39caa230
VMatti00 <43467719+VMatti00@users.noreply.github.com>
2024-10-01 10:13:15 +0200

    Le big finale

--- src/main.c
+++ src/main.c
@@ -16,17 +16,7 @@
 #define FONT_SRC "./resources/arial.ttf"
 #define FONT_SIZE 64
 #define HIGHSCORE_FILE "./resources/highscores.txt"
-// #define WINDOW_WIDTH 500
-// #define WINDOW_HEIGHT 500
 
-/*
-TODO: create function createSnake and drawSnake
-        write the functions in snake.c and snake.h file
-        link the snake.c and snake.h in the make file
-
-TODO: write the handle inputs
-TODO: check if you need the SDL_Surface and textrs
-*/
 
 typedef struct highscore {
     char name[50];
@@ -42,8 +32,12 @@ struct game
     Board *pBoard;
     Food *pFood;
     TTF_Font *font;
-    // SDL_Rect gameRect;
-    // int score;
+    SDL_Texture *scoreTexture;
+    SDL_Texture *aiScoreTexture;
+    int scoreTextWidth;
+    int scoreTextHeight;
+    int aiScoreTextWidth;
+    int aiScoreTextHeight;
 };
 typedef struct game Game;
 
@@ -55,6 +49,7 @@ void mainMenu(Game *pGame);
 void showHighScore(Game *pGame);
 void saveScore(const char *playerName, int score);
 void drawMenu(Game *pGame);
+void updateScore(Snake *pSnake, Game *pGame, int isAi);
 
 
 int checkCollision(SDL_Rect pSnake, SDL_Rect pFood);
@@ -65,7 +60,6 @@ int main(int argc, char *argv[])
     Game g = {0};
     if (!initiate(&g)) return 1;
     drawMenu(&g);
-    // run_game(&g);
     close_game(&g);
     return 0;
 }
@@ -99,8 +93,6 @@ int initiate(Game *pGame)
     pGame->pSnake = createSnake(pGame->pRenderer, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
     pGame->pSnakeAI = createSnake(pGame->pRenderer, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
     pGame->pFood = createFood(pGame->pRenderer, getBoardWidth(pGame->pBoard), getBoardHeight(pGame->pBoard), getBoardX(pGame->pBoard), getBoardY(pGame->pBoard));
-    // pGame->score = 0;
-    
 
     return 1;
 }
@@ -146,12 +138,10 @@ void drawMenu(Game *pGame) {
 
     SDL_Color white = {255, 255, 255, 255};
 
-    // Render the menu background
     SDL_Rect menuRect = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
     SDL_SetRenderDrawColor(pGame->pRenderer, 0, 0, 0, 255);
     SDL_RenderFillRect(pGame->pRenderer, &menuRect);
 
-    // Render the "Play" option
     SDL_Surface *playSurface = TTF_RenderText_Solid(font, "Play", white);
     SDL_Texture *playTexture = SDL_CreateTextureFromSurface(pGame->pRenderer, playSurface);
     int playTextWidth, playTextHeight;
@@ -159,7 +149,6 @@ void drawMenu(Game *pGame) {
     SDL_Rect playTextRect = {WINDOW_WIDTH / 2 - playTextWidth / 2, WINDOW_HEIGHT / 2 - 50 + (50 - playTextHeight) / 2, playTextWidth, playTextHeight};
     SDL_RenderCopy(pGame->pRenderer, playTexture, NULL, &playTextRect);
 
-    // Render the "High Scores" option
     SDL_Surface *highScoreSurface = TTF_RenderText_Solid(font, "High Scores", white);
     SDL_Texture *highScoreTexture = SDL_CreateTextureFromSurface(pGame->pRenderer, highScoreSurface);
     int highScoreTextWidth, highScoreTextHeight;
@@ -169,13 +158,11 @@ void drawMenu(Game *pGame) {
 
     SDL_RenderPresent(pGame->pRenderer);
 
-    // Free the surfaces and textures
     SDL_FreeSurface(playSurface);
     SDL_FreeSurface(highScoreSurface);
     SDL_DestroyTexture(playTexture);
     SDL_DestroyTexture(highScoreTexture);
 
-    // Close the font
     TTF_CloseFont(font);
 
     int close_requested = 0;
@@ -189,14 +176,12 @@ void drawMenu(Game *pGame) {
                 int x, y;
                 SDL_GetMouseState(&x, &y);
 
-                // Check if the "Play" option was clicked
                 if (x >= playTextRect.x && x <= playTextRect.x + playTextRect.w &&
                     y >= playTextRect.y && y <= playTextRect.y + playTextRect.h) {
                     run_game(pGame);
                     close_requested = 1;
                 }
 
-                // Check if the "High Scores" option was clicked
                 if (x >= highScoreTextRect.x && x <= highScoreTextRect.x + highScoreTextRect.w &&
                     y >= highScoreTextRect.y && y <= highScoreTextRect.y + highScoreTextRect.h) {
                     showHighScore(pGame);
@@ -218,16 +203,13 @@ void showHighScore(Game *pGame) {
 
     SDL_Color white = {255, 255, 255, 255};
 
-    // Render the high scores background
     SDL_Rect highScoreRect = {0, 0, WINDOW_WIDTH, WINDOW_HEIGHT};
     SDL_SetRenderDrawColor(pGame->pRenderer, 0, 0, 0, 255);
     SDL_RenderFillRect(pGame->pRenderer, &highScoreRect);
 
-    // Read high scores from file
     int numScores;
     HighScore *scores = readHighScores(&numScores);
 
-    // Render each high score
     for (int i = 0; i < numScores; ++i) {
         char scoreText[100];
         snprintf(scoreText, sizeof(scoreText), "%s - %d", scores[i].name, scores[i].score);
@@ -261,10 +243,6 @@ void showHighScore(Game *pGame) {
 
     SDL_RenderPresent(pGame->pRenderer);
 
-    // Free the font and scores
-    TTF_CloseFont(font);
-    free(scores);
-
     int close_requested = 0;
     SDL_Event eventGame;
 
@@ -276,7 +254,6 @@ void showHighScore(Game *pGame) {
                 int x, y;
                 SDL_GetMouseState(&x, &y);
 
-                // Check if the user clicked to go back to the menu
                 if (x >= 0 && x <= WINDOW_WIDTH && y >= 0 && y <= WINDOW_HEIGHT) {
                     drawMenu(pGame);
                     close_requested = 1;
@@ -292,7 +269,10 @@ void endGame(Game *pGame, const char *playerName, int score) {
     
     pGame->pSnake = createSnake(pGame->pRenderer, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
     pGame->pSnakeAI = createSnake(pGame->pRenderer, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
-
+    SDL_DestroyTexture(pGame->scoreTexture);
+    pGame->scoreTexture = NULL;
+    SDL_DestroyTexture(pGame->aiScoreTexture);
+    pGame->aiScoreTexture = NULL;
     drawMenu(pGame);
 }
 
@@ -307,8 +287,7 @@ void run_game(Game *pGame)
     while (!close_requested){
         while (SDL_PollEvent(&eventGame)){
             if (eventGame.type == SDL_QUIT) close_requested = 1;
-            else 
-            {
+            else {
                 handleInput(pGame, &eventGame);
             }
         }
@@ -321,31 +300,38 @@ void run_game(Game *pGame)
             printf("Game over\n");
             break;
         }
-        // updateSnake(pGame->pSnake, getBoardWidth(pGame->pBoard), getBoardHeight(pGame->pBoard), getBoardX(pGame->pBoard), getBoardY(pGame->pBoard));
         SDL_Rect snakeHeadRectAI = pGame->pSnakeAI->pHead->rect;
         SDL_Rect snakeHeadRect = pGame->pSnake->pHead->rect;
         SDL_Rect foodRect = pGame->pFood->rect;
         if(checkCollision(snakeHeadRect, foodRect)){
             growSnake(pGame->pSnake);
             repositionFood(pGame->pFood, getBoardWidth(pGame->pBoard), getBoardHeight(pGame->pBoard), getBoardX(pGame->pBoard), getBoardY(pGame->pBoard));
-            // pGame->score++;
+            updateScore(pGame->pSnake, pGame,0);
+
         }
         else if(checkCollision(snakeHeadRectAI, foodRect)){
             growSnake(pGame->pSnakeAI);
             repositionFood(pGame->pFood, getBoardWidth(pGame->pBoard), getBoardHeight(pGame->pBoard), getBoardX(pGame->pBoard), getBoardY(pGame->pBoard));
+            updateScore(pGame->pSnakeAI, pGame, 1);
         }
-        // printf("Snake x: %d, y: %d\n", pGame->pSnake->pHead->rect.x, pGame->pSnake->pHead->rect.y);
 
         SDL_SetRenderDrawColor(pGame->pRenderer, 0, 0, 0, 255);
         SDL_RenderClear(pGame->pRenderer);
 
-
-
-        // SDL_SetRenderDrawColor(pGame->pRenderer, 255, 255, 255, 255);
         drawBoard(pGame->pBoard);
         drawSnake(pGame->pSnakeAI);
         drawSnake(pGame->pSnake);
         drawFood(pGame->pFood);
+
+        
+        if (pGame->aiScoreTexture) {
+            SDL_Rect aiTextRect = {10, 10, pGame->aiScoreTextWidth, pGame->aiScoreTextHeight};
+            SDL_RenderCopy(pGame->pRenderer, pGame->aiScoreTexture, NULL, &aiTextRect);
+        }
+        if (pGame->scoreTexture) {
+            SDL_Rect userTextRect = {WINDOW_WIDTH - pGame->scoreTextWidth - 10, 10, pGame->scoreTextWidth, pGame->scoreTextHeight};
+            SDL_RenderCopy(pGame->pRenderer, pGame->scoreTexture, NULL, &userTextRect);
+        }
 
         SDL_RenderPresent(pGame->pRenderer);
         SDL_Delay(16);
@@ -356,10 +342,7 @@ void run_game(Game *pGame)
     const char playerName[50];
     scanf("%s", playerName);
 
-    
-
     endGame(pGame, playerName, pGame->pSnake->length);
-    // SDL_DestroyWindow(pGame->pWindow);
 }
 
 
@@ -401,4 +384,40 @@ void close_game(Game *pGame)
     if (pGame->pRenderer) SDL_DestroyRenderer(pGame->pRenderer);
     if (pGame->pWindow) SDL_DestroyWindow(pGame->pWindow);
     SDL_Quit();
+}
+
+void updateScore(Snake *pSnake, Game *pGame, int isAI){
+    TTF_Font *font = TTF_OpenFont(FONT_SRC, FONT_SIZE);
+    if (!font) {
+        printf("TTF_OpenFont: %s\n", TTF_GetError());
+        return;
+    }
+
+
+    SDL_Color white = {255, 255, 255, 255};
+    char scoreText[100];
+    snprintf(scoreText, sizeof(scoreText), "Score: %d", pSnake->length-1);
+
+    SDL_Surface *surface = TTF_RenderText_Solid(font, scoreText, white);
+    if (!surface) {
+        printf("TTF_RenderText_Solid: %s\n", TTF_GetError());
+        return;
+    }
+
+    SDL_Texture *texture = SDL_CreateTextureFromSurface(pGame->pRenderer, surface);
+    if (!texture) {
+        printf("SDL_CreateTextureFromSurface: %s\n", SDL_GetError());
+        SDL_FreeSurface(surface);
+        return;
+    }
+
+    if (isAI) {
+        pGame->aiScoreTexture = texture;
+        SDL_QueryTexture(texture, NULL, NULL, &pGame->aiScoreTextWidth, &pGame->aiScoreTextHeight);
+    } else {
+        pGame->scoreTexture = texture;
+        SDL_QueryTexture(texture, NULL, NULL, &pGame->scoreTextWidth, &pGame->scoreTextHeight);
+    }
+    
+    SDL_FreeSurface(surface);
 }
